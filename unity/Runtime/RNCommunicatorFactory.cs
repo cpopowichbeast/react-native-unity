003cb6631cd6a3394119b9dbdfe519ca1e3984aa
Christopher Popowich <CPopowich@driveable.com>
2020-07-27 09:01:38 -0600

    Removed dependency to Mock class in communicator factory

--- unity/Runtime/RNCommunicatorFactory.cs
+++ unity/Runtime/RNCommunicatorFactory.cs
@@ -2,10 +2,15 @@ namespace Wowmaking.RNU
 {
     public static class RNCommunicatorFactory
     {
+        private static IRNCommunicator mockCommunicator;
+        public static void SetMockCommunicator(IRNCommunicator mockCommunicator)
+        {
+            this.mockCommunicator = mockCommunicator;
+        }
         public static IRNCommunicator GetCommunicator()
         {
 #if UNITY_EDITOR
-            return new MockRNCommunicator();
+            return new mockCommunicator;
 #elif UNITY_IOS
             return new IOSRNCommunicator();
 #elif UNITY_ANDROID
